1c8b4a36acae5909ee4f6b5e80dc2ae78c036271
johess123 <s109213067@mail1.ncnu.edu.tw>
2026-02-15 19:49:26 +0800

    支援對多4人對戰

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ import socketio
 sio = socketio.Server(cors_allowed_origins='*', async_mode='eventlet')
 app = socketio.WSGIApp(sio)
 
-all_player = [['',''] for i in range(10)] # 10 個房間的玩家
-play_ready = [[-1,-1] for i in range(10)] # 已經準備開始
-player_best_score = [['',''] for i in range(10)] # 玩家最佳分數
-player_win = [['',''] for i in range(10)] # 玩家勝場數
-player_lose = [['',''] for i in range(10)] # 玩家敗場數
-player_tie = [['',''] for i in range(10)] # 玩家平手數
+all_player = [['','','',''] for i in range(10)] # 10 個房間的玩家，每房最多 4 人
+play_ready = [[-1,-1,-1,-1] for i in range(10)] # 已經準備開始
+player_best_score = [['','','',''] for i in range(10)] # 玩家最佳分數
+player_win = [['','','',''] for i in range(10)] # 玩家勝場數
+player_lose = [['','','',''] for i in range(10)] # 玩家敗場數
+player_tie = [['','','',''] for i in range(10)] # 玩家平手數
 
 lock_room = [False for i in range(10)] # 房間開始遊戲後不可進
 
@@ -40,12 +40,14 @@ def register_player(sid, data): # 玩家進入房間
         if uid in all_player[i]:
             try:
                 old_idx = all_player[i].index(uid)
-                if old_idx < 2:
+                if old_idx < 4:
                     all_player[i][old_idx] = ''
                     play_ready[i][old_idx] = -1
                 else:
                     all_player[i].remove(uid)
-                if all_player[i][0] == '' and all_player[i][1] == '':
+                
+                # 若房內無玩家(0-3)了，則解鎖且其餘人移除
+                if all(p == '' for p in all_player[i][0:4]):
                     lock_room[i] = False
                     gameover_count[i] = 0
             except ValueError:
@@ -55,20 +57,20 @@ def register_player(sid, data): # 玩家進入房間
     name_to_sid[sid] = uid 
     uid_to_sid[uid] = sid
     
-    # 3. 進入目標房間 (前兩名為玩家，其餘為觀戰)
+    # 3. 進入目標房間 (前四名為玩家，其餘為觀戰)
     p_idx = -1
-    if all_player[room_num][0] == '':
-        all_player[room_num][0] = uid
-        p_idx = 0
-    elif all_player[room_num][1] == '':
-        all_player[room_num][1] = uid
-        p_idx = 1
-    else:
+    for i in range(4):
+        if all_player[room_num][i] == '':
+            all_player[room_num][i] = uid
+            p_idx = i
+            break
+    
+    if p_idx == -1:
         all_player[room_num].append(uid)
         p_idx = len(all_player[room_num]) - 1
     
     # 更新統計資訊
-    if p_idx != -1 and p_idx < 2:
+    if p_idx != -1 and p_idx < 4:
         player_best_score[room_num][p_idx] = data['best_score']
         player_win[room_num][p_idx] = data['player_win']
         player_lose[room_num][p_idx] = data['player_lose']
@@ -93,22 +95,25 @@ def leave_room(sid,data): # 玩家退出房間
     global name_to_sid, uid_to_sid
     
     try:
-        player_index = all_player[room_num].index(uid)
-        if player_index < 2:
-            all_player[room_num][player_index] = ''
-            play_ready[room_num][player_index] = -1
-            player_best_score[room_num][player_index] = ''
-            player_win[room_num][player_index] = ''
-            player_lose[room_num][player_index] = ''
-            player_tie[room_num][player_index] = ''
-        else:
-            all_player[room_num].remove(uid)
+        player_list = all_player[room_num]
+        if uid in player_list:
+            player_index = player_list.index(uid)
+            if player_index < 4:
+                all_player[room_num][player_index] = ''
+                play_ready[room_num][player_index] = -1
+                player_best_score[room_num][player_index] = ''
+                player_win[room_num][player_index] = ''
+                player_lose[room_num][player_index] = ''
+                player_tie[room_num][player_index] = ''
+            else:
+                all_player[room_num].remove(uid)
             
     except ValueError:
         pass
     
     sio.leave_room(sid, room_num)
-    if all_player[room_num][0] == '' and all_player[room_num][1] == '': # 房間沒人了
+    # 檢查是否房內無玩家了
+    if all(p == '' for p in all_player[room_num][0:4]):
         lock_room[room_num] = False
         gameover_count[room_num] = 0
         sio.emit('lock_room',{'lock_room_list': lock_room})
@@ -121,7 +126,7 @@ def leave_room(sid,data): # 玩家退出房間
         'player_win': player_win[room_num],
         'player_lose': player_lose[room_num],
         'player_tie': player_tie[room_num],
-        'player_index': -1 # Leave room doesn't need to specify who joined
+        'player_index': -1
     }, room=room_num)
 
 @sio.event
@@ -129,11 +134,15 @@ def room_ready(sid, data): # 房間準備開始
     uid = data['uid']
     room_num = data['room_num']
     p_idx = data['player_index']
-    if p_idx < 2:
+    if p_idx < 4:
         play_ready[room_num][p_idx] = data['level']
-        if play_ready[room_num][0] != -1 and play_ready[room_num][1] != -1:
+        
+        # 檢查是否可以開始 (至少兩人，且所有現有玩家都 ready)
+        players_in_slots = [i for i, name in enumerate(all_player[room_num][0:4]) if name != '']
+        if len(players_in_slots) >= 2 and all(play_ready[room_num][i] != -1 for i in players_in_slots):
             lock_room[room_num] = True
             sio.emit('lock_room',{'lock_room_list': lock_room})
+            
         sio.emit('play_ready',{'ready_level': play_ready[room_num]}, room=room_num)
 
 @sio.event
@@ -147,16 +156,27 @@ def tetris_server_block(sid, data):
     sio.emit('tetris_client_block', data, room=room_num, skip_sid=sid)
 
 @sio.event
+def request_gameover_sync(sid, data):
+    room_num = data['room_num']
+    # Once requested, force everyone in the room to gameover state
+    play_ready[room_num] = [-1,-1,-1,-1]
+    lock_room[room_num] = False
+    gameover_count[room_num] = 0
+    sio.emit('gameover_sync', {}, room=room_num)
+    sio.emit('lock_room', {'lock_room_list': lock_room})
+
+@sio.event
 def gameover(sid,data):
     room_num = data['room_num']
     gameover_count[room_num] = gameover_count[room_num] + 1
-    # 只要有人掛掉就廣播，不論是不是雙方都掛掉
     sio.emit('client_gameover', data, room=room_num, skip_sid=sid)
     
-    if gameover_count[room_num] >= 2: # 雙方都掛掉，遊戲重置房間狀態
-        play_ready[room_num] = [-1,-1]
+    players_count = len([name for name in all_player[room_num][0:4] if name != ''])
+    if gameover_count[room_num] >= players_count: # 所有玩家都掛掉
+        play_ready[room_num] = [-1,-1,-1,-1]
         lock_room[room_num] = False
         gameover_count[room_num] = 0
+        sio.emit('gameover_sync', {}, room=room_num) # Final insurance
         sio.emit('lock_room',{'lock_room_list': lock_room})
 
 @sio.event
@@ -170,7 +190,7 @@ def disconnect(sid):
                 if uid in all_player[i]:
                     room_num = i
                     player_index = all_player[room_num].index(uid)
-                    if player_index < 2:
+                    if player_index < 4:
                         all_player[room_num][player_index] = ''
                         play_ready[room_num][player_index] = -1
                         player_best_score[room_num][player_index] = ''
@@ -179,6 +199,7 @@ def disconnect(sid):
                         player_tie[room_num][player_index] = ''
                     else:
                         all_player[room_num].remove(uid)
+                    
                     sio.emit('in_room', {
                         'player_name': all_player[room_num],
                         'ready_level': play_ready[room_num],
@@ -188,7 +209,8 @@ def disconnect(sid):
                         'player_lose': player_lose[room_num],
                         'player_tie': player_tie[room_num]
                     }, room=room_num)
-                    if all_player[room_num][0] == '' and all_player[room_num][1] == '': 
+                    
+                    if all(p == '' for p in all_player[room_num][0:4]):
                         lock_room[room_num] = False
                         gameover_count[room_num] = 0
                         sio.emit('lock_room',{'lock_room_list': lock_room})
